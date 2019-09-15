5115cd4684034891d0162d4fc99f6146ae1910d7
shahrukhktk <shahrukhktk760@gmail.com>
2019-09-15 15:52:13 +0500

    card design

--- lib/main.dart
+++ lib/main.dart
@@ -8,57 +8,133 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          backgroundColor: Colors.blueGrey,
-          title: Text('Card App'),
-        ),
+        backgroundColor: Colors.red,
+//        appBar: AppBar(
+//          backgroundColor: Colors.teal,
+//          title: Text('Card App'),
+//        ),
         body: SafeArea(
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
+          child: Column(
             children: <Widget>[
-              Expanded(
-                child: Container(
-                  height: 100,
-                  width: 100,
-                  margin: EdgeInsets.all(10),
-                  color: Colors.blueGrey,
-                  child: Icon(
-                    Icons.star,
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('images/srk.jpg'),
+              ),
+              Text(
+                'ShahRukh',
+                style: TextStyle(
+                    fontSize: 30,
                     color: Colors.white,
-                  ),
+                    fontStyle: FontStyle.normal,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 3,
+                    fontFamily: 'pacifico'),
+              ),
+              Text(
+                'Flutter Developer',
+                style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                    fontFamily: 'Titillium',
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2.5),
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                padding: EdgeInsets.all(15),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.phone,
+                      color: Colors.red,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      '+92 313-0141046',
+                      style: TextStyle(
+                        fontSize: 15.0,
+                        color: Colors.red,
+                      ),
+                    )
+                  ],
                 ),
               ),
-//              SizedBox(
-//                height: 5,
-//              ),
-              Expanded(
-                flex: 2,
-                child: Container(
-                  height: 100,
-                  width: 100,
-                  margin: EdgeInsets.all(10),
-                  color: Colors.blueGrey,
-                  child: Icon(
-                    Icons.star,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  height: 100,
-                  width: 100,
-                  margin: EdgeInsets.all(10),
-                  color: Colors.blueGrey,
-                  child: Icon(
-                    Icons.star,
-                    color: Colors.white,
-                  ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                padding: EdgeInsets.all(15),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                      color: Colors.red,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      'Shahrukhktk760@gmail.com',
+                      style: TextStyle(
+                        fontSize: 15.0,
+                        color: Colors.red,
+                      ),
+                    )
+                  ],
                 ),
               )
             ],
           ),
+//          child: Row(
+//            crossAxisAlignment: CrossAxisAlignment.center,
+//            children: <Widget>[
+//              Expanded(
+//                child: Container(
+//                  height: 100,
+//                  width: 100,
+//                  margin: EdgeInsets.all(10),
+//                  color: Colors.blueGrey,
+//                  child: Icon(
+//                    Icons.star,
+//                    color: Colors.white,
+//                  ),
+//                ),
+//              ),
+////              SizedBox(
+////                height: 5,
+////              ),
+//              Expanded(
+//                flex: 2,
+//                child: Container(
+//                  height: 100,
+//                  width: 100,
+//                  margin: EdgeInsets.all(10),
+//                  color: Colors.blueGrey,
+//                  child: Icon(
+//                    Icons.star,
+//                    color: Colors.white,
+//                  ),
+//                ),
+//              ),
+//              Expanded(
+//                child: Container(
+//                  height: 100,
+//                  width: 100,
+//                  margin: EdgeInsets.all(10),
+//                  color: Colors.blueGrey,
+//                  child: Icon(
+//                    Icons.star,
+//                    color: Colors.white,
+//                  ),
+//                ),
+//              )
+//            ],
+//          ),
         ),
       ),
     );
